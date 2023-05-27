90e17ccfe96055d395a92f51191de974aeb1cc67
PrernaSharma7 <106103614+PrernaSharma7@users.noreply.github.com>
2023-05-27 18:17:41 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.print("Enter width:");
+        double width= input.nextDouble();
+        System.out.print("Enter height:");
+        double height= input.nextDouble();
+        double area = height*width;
+        System.out.println("Area of the Rectangle is: "+area);
+    }
+}
